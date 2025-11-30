a41918278c95fd08f86d27e772846da0d459544a
kami-nogueira <kamila_nogueiraoutlook.com git config --global user.name kami-nogueiragit config --global user.email kamila_nogueira@outlook.com>
2025-11-30 18:31:23 +0000

    Valores duplicados deletados das tabelas

---
+++ 04_deleteTable.sql
@@ -0,0 +1,32 @@
+select * from livro;
+
+
+select DISTINCT titulo
+from livro;
+
+Delete from Livro
+where id_livro = 4;
+
+Delete from Livro
+where id_livro = 5;
+
+Delete from Livro
+where id_livro = 6;
+
+Delete from Livro
+where id_livro = 7;
+
+Delete from Livro
+where id_livro = 8;
+
+Delete from Livro
+where id_livro = 9;
+
+Delete from Livro
+where id_livro = 10;
+
+Delete from Livro
+where id_livro = 11;
+
+Delete from Livro
+where id_livro = 12;
